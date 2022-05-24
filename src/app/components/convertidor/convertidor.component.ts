4be8813fdc31de0cfe50f1d4aef05154332ddd70
Jhonnatan Dussan <jhonnat.dussan@gmail.com>
2022-05-23 23:01:39 -0500

    changes

--- src/app/components/convertidor/convertidor.component.ts
+++ src/app/components/convertidor/convertidor.component.ts
@@ -5,14 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './convertidor.component.html',
   styleUrls: ['./convertidor.component.css']
 })
+
 export class ConvertidorComponent implements OnInit {
+
   cantidad = 0;
   tengo = 'USD';
   quiero = 'EUR';
   total = 0;
 
-  monedas: string[] = ['USD', 'EUR', 'PES']
-
+  monedas: string[] = ['USD', 'EUR', 'PES'];
 
   constructor() { }
 
